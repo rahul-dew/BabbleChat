2d54c107f2a2ef5b9df0201dabf8676f7203f60f
rahul-dew <rahuldewangan.rd@gmail.com>
2021-05-29 04:38:29 +0530

    Implemented Main UI

--- app/src/main/java/com/overlord/babblechat/MainActivity.java
+++ app/src/main/java/com/overlord/babblechat/MainActivity.java
@@ -2,20 +2,97 @@ package com.overlord.babblechat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.overlord.babblechat.profile.ProfileActivity;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabMain);
+        viewPager = findViewById(R.id.vpMain);
+
+        setViewPager();
+    }
+
+    class Adapter extends FragmentPagerAdapter{
+
+        public Adapter(@NonNull @NotNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    ChatFragment chatFragment = new ChatFragment();
+                    return chatFragment;
+                case 1:
+                    RequestsFragment requestsFragment = new RequestsFragment();
+                    return requestsFragment;
+                case 2:
+                    FindFriendFragment findFriendFragment = new FindFriendFragment();
+                    return findFriendFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return tabLayout.getTabCount();
+        }
+    }
+
+    private void setViewPager(){
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_chat));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_request));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_find_friend));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        Adapter adapter = new Adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 
     @Override
@@ -31,5 +108,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean doubleBackPressed = false;
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if(tabLayout.getSelectedTabPosition()>0){
+            tabLayout.selectTab(tabLayout.getTabAt(0));
+        }
+        else{
+            if(doubleBackPressed){
+                finishAffinity();
+            }
+            else{
+                doubleBackPressed = true;
+                Toast.makeText(MainActivity.this, getString(R.string.back_to_exit), Toast.LENGTH_SHORT).show();
+
+                //delay
+                android.os.Handler handler = new android.os.Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doubleBackPressed = false;
+                    }
+                }, 2000);
+            }
+        }
     }
 }
